8ba9ccebf8d8e9845139a866135215c97d512d33
mikhail.khinevich <mikhail.khinevich@tum.de>
2025-05-12 23:46:14 +0200

    initial project commit

---
+++ iOS/Steezy_pyswift/Steezy_pyswift/Steezy_pyswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steezy_pyswiftApp.swift
+//  Steezy_pyswift
+//
+//  Created by Mikhail Khinevich on 12.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Steezy_pyswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
